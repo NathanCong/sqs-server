71276b219f1171aa6b09f0e0d394299e1caf0318
NathanCong <congyunan@126.com>
2025-10-19 14:46:20 +0800

    feat: 优化检索策略生成服务

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,9 +20,9 @@ router.post('/analysis/semantics', AnalysisSemanticsController.index);
 router.post('/helper/disclosure/stream', HelperDisclosureStreamController.index);
 // 大模型 - 专利撰写接口
 router.post('/helper/patent/stream', HelperPatentStreamController.index);
-// 大模型 - 检索策略接口
+// 大模型 - 检索策略接口（已经优化）
 router.post('/search/strategy', SearchStrategyController.index);
-// 万象云 - 检索专利接口
+// 万象云 - 检索专利接口（已经优化）
 router.post('/search/patents', SearchPatentsController.index);
 
 export default router;
